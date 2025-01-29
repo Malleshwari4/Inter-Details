6037666e8dd569ed318782ce0198d7ca939358b6
Malleshwari4 <pallikondamalleshwari@gmail.com>
2025-01-29 15:14:15 +1300

    first commit

---
+++ src/test/java/com/Details/Malleshwari.java
@@ -0,0 +1,12 @@
+package com.Details;
+
+public class Malleshwari {
+
+	public static void main(String[] args) {
+		System.out.println("This is Malleshwari");
+		System.out.println("I have completed my inter in Sri Vijay sai ,Bhodhan");
+		System.out.println("My intermediate marks are 840");
+		
+	}
+
+}
