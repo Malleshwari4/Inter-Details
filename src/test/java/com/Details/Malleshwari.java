70fd634990383e13a19dde7daf65a3ce211baac9
Pasunuri-Raghu <psnr.raghu@gmail.com>
2025-01-30 13:11:27 +0530

    this raghu tester add my class successfully

--- src/test/java/com/Details/Malleshwari.java
+++ src/test/java/com/Details/Malleshwari.java
@@ -6,7 +6,7 @@ public class Malleshwari {
 		System.out.println("This is Malleshwari");
 		System.out.println("I have completed my inter in Sri Vijay sai ,Bhodhan");
 		System.out.println("My intermediate marks are 840");
-		
+		System.out.println("Thsi is tester@ raghu, i checked out your class");
 	}
 
 }
